0b8f123c50f4450ed9def6293bd84ba8b6552ee0
Raphael Saint-Louis <raphaelstlouis@gmail.com>
2017-09-04 21:33:06 -0400

    added manual session termination

--- client.c
+++ client.c
@@ -9,30 +9,30 @@
 
 int main(int argc, char *argv[])
 {
-	struct addrinfo hints, *res;
+	struct addrinfo hints = {0};
+	struct addrinfo *response = NULL;
 	int status;
 	char ipstring[INET_ADDRSTRLEN];
 	
-	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	
-	status = getaddrinfo("localhost", "4444", &hints, &res);
+	status = getaddrinfo("localhost", "4444", &hints, &response);
 	
 	if (status != 0) {
 		printf("error in getaddrinfo(): %s\n", gai_strerror(status));
 		return 0;
 	}
 	
-	struct sockaddr_in *addr = (struct sockaddr_in *)res->ai_addr;
+	struct sockaddr_in *addr = (struct sockaddr_in *)response->ai_addr;
 	inet_ntop(AF_INET, (void *)&(addr->sin_addr), ipstring, INET_ADDRSTRLEN);
 	
-	int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	int sockfd = socket(response->ai_family, response->ai_socktype, response->ai_protocol);
 	if (sockfd == -1) {
 		printf("error in socket creation()\n");
 	};
 	
-	status = connect(sockfd, res->ai_addr, res->ai_addrlen);
+	status = connect(sockfd, response->ai_addr, response->ai_addrlen);
 	if (status != 0) {
 		perror("error");
 		return -1;
@@ -45,5 +45,6 @@ int main(int argc, char *argv[])
 	fgets(msg_buffer, MESSAGE_MAX_LEN, stdin);
 	
 	status = send(sockfd, (void *)msg_buffer, MESSAGE_MAX_LEN, 0);
+		
 	return 0;
 }
--- server.c
+++ server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -10,43 +11,43 @@
 
 int main(int argc, char *argv[])
 {
-	struct addrinfo hints, *res;
+	struct addrinfo hints = {0};
+	struct addrinfo *response = NULL;
 	int status;
 	char ipstring[INET_ADDRSTRLEN];
 	struct sockaddr_storage incoming;
 	
-	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	
 	// only working locally for now
-	status = getaddrinfo("localhost", "4444", &hints, &res);
+	status = getaddrinfo("localhost", "4444", &hints, &response);
 	
 	if (status != 0) {
 		printf("error in getaddrinfo(): %s\n", gai_strerror(status));
 		return 0;
 	}
 	
-	struct sockaddr_in *addr = (struct sockaddr_in *)res->ai_addr;
+	struct sockaddr_in *addr = (struct sockaddr_in *)response->ai_addr;
 	inet_ntop(AF_INET, (void *)&(addr->sin_addr), ipstring, INET_ADDRSTRLEN);
-	
-	int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+	int sockfd = socket(response->ai_family, response->ai_socktype, response->ai_protocol);
 	if (sockfd == -1) {
 		printf("error in socket creation()\n");
 	};
 	
-	int bindstatus;
-	int listenstatus;
+	int bindStatus;
+	int listenStatus;
 
 	// error checking
-	bindstatus = bind(sockfd, res->ai_addr, res->ai_addrlen);
-	if (bindstatus != 0) {
+	bindStatus = bind(sockfd, response->ai_addr, response->ai_addrlen);
+	if (bindStatus != 0) {
 		perror("error"); 
 		return -1;
 	}
 
-	listenstatus = listen(sockfd, CONNECTION_QUEUE_MAX);
-	if (listenstatus != 0) {
+	listenStatus = listen(sockfd, CONNECTION_QUEUE_MAX);
+	if (listenStatus != 0) {
 		perror("error"); 
 		return -1;
 	}
@@ -55,13 +56,20 @@ int main(int argc, char *argv[])
 	socklen_t size = sizeof(incoming);
 	printf("listening on: %s\n", ipstring);
 
+	char *exitText = "exit";
+	size_t exitTextLen = strlen(exitText);
+
 	while (0 != 1) {
 		connectfd = accept(sockfd, (struct sockaddr *)&incoming, &size);
-
-		char msg_buffer[MESSAGE_MAX_LEN];
+		char msg_buffer[MESSAGE_MAX_LEN] = "";
 		status = recv(connectfd, (void *)msg_buffer, MESSAGE_MAX_LEN, 0);
 
 		printf("message received: %s", msg_buffer);
+		if (strncmp(msg_buffer, exitText, exitTextLen) == 0) {
+			printf("action: terminating connection\n");
+			close(sockfd);
+			break;
+		}
 	}
 
 	return status;
